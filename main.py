c246fcb616b3b52efd9691d04b2874514a70897c
BlueBM <100818726+BlueBM@users.noreply.github.com>
2022-03-17 13:46:10 +1100

    test

--- main.py
+++ main.py
@@ -13,12 +13,62 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 
 
 # Create your objects here.
+
 ev3 = EV3Brick()
 
+#motors
 motorA = Motor(Port.A)
 motorB = Motor(Port.B)
+
+#colour sensors
+left_sensor = ColorSensor(Port.S3)
+right_sensor = ColorSensor(Port.S2)
+
+#drive base                                     (change)            (check?)
+robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+
+# Calculate the light threshold. Choose values based on your measurements.
+BLACK = 9
+WHITE = 85
+threshold = (BLACK + WHITE) / 2
+
+
+# Set the drive speed at 100 millimeters per second.
+DRIVE_SPEED = 100
+
+# Set the gain of the proportional line controller. This means that for every
+# percentage point of light deviating from the threshold, we set the turn
+# rate of the drivebase to 1.2 degrees per second.
+
+# For example, if the light value deviates from the threshold by 10, the robot
+# steers at 10*1.2 = 12 degrees per second.
+PROPORTIONAL_GAIN = 1.2
+
+
+
+
+
 # Write your program here.
+
 def startup():
     ev3.speaker.say("Initialising Startup")
     ev3.speaker.say("test")
 
+
+
+
+while True:
+    # Calculate the deviation from the threshold.
+    deviation = line_sensor.reflection() - threshold
+
+    # Calculate the turn rate.
+    turn_rate = PROPORTIONAL_GAIN * deviation
+
+    # Set the drive base speed and turn rate.
+    robot.drive(DRIVE_SPEED, turn_rate)
+
+    # You can wait for a short time or do other things in this loop.
+    wait(10)
+
+
+    #   i copied from stuff so
