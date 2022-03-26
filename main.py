32c13939ac5936a14d43d2c6a1bcb735d95a6eb3
will-da-pro <56065795+will-da-pro@users.noreply.github.com>
2022-03-26 11:01:04 +1100

    um
    @ school

--- main.py
+++ main.py
@@ -15,54 +15,79 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 # Create your objects here.
 ev3 = EV3Brick()
 
-lMotor = Motor(Port.A)
-rMotor = Motor(Port.D)
+
 claw = Motor(Port.C)
-lColour = ColorSensor.reflection(ColorSensor(Port.S1))
-rColour = ColorSensor.reflection(ColorSensor(Port.S4))
 ultraS = UltrasonicSensor(Port.S3)
 robot = DriveBase(lMotor, rMotor, wheel_diameter=55.5, axle_track=104) #to check next week
 
-BLACK = 0 - 20
-WHITE = 20 - 90
+
 SILVER = 90 - 100
+
+lMotor = Motor(Port.A)
+rMotor = Motor(Port.D)
+    
 DRIVE_SPEED = 100
+
+
 # Write your program here.
-def findPath():
-    robot.stop()
-    time = 0
-    while True:
-        if time >= 360:
-            return False
-        robot.turn(1, True)
-        if lColour == BLACK or rColour == BLACK:
-            return True
-        time += 1
-            
+
+def calib():
+    lColour = ColorSensor(Port.S1)
+    rColour = ColorSensor(Port.S4)
+    while not any(ev3.buttons.pressed()):
+        continue
+    wait(1000)
+    WHITE = color_sensor.reflection(Port.S1)
+    ev3.speaker.beep()
+    print("white colour:", WHITE)
+    
+    while not any(ev3.buttons.pressed()):
+        continue
+    wait(1000)
+    BLACK = color_sensor.reflection(Port.S1)
+    ev3.speaker.beep()
+    print("black colour:", BLACK)
+    
+
+
+
+
+#def findPath():
+ #   robot.stop()
+  #  time = 0
+   # while True:
+    #    if time >= 360:
+     #       return False
+      #  robot.turn(1, True)
+       # if lColour == BLACK or rColour == BLACK:
+        #    return True
+        #time += 1       
         
-        
-def move():
-    while True:
-        if lColour == BLACK and rColour == BLACK:
-            robot.drive(DRIVE_SPEED)
-        elif not lColour == BLACK and not rColour == BLACK:
-            path = findPath()
-            if path:
-                pass
-            else:
-                break
-        while lColour == BLACK and rColour == BLACK:
-            pass
-def startup():
-    ev3.speaker.say("Initialising Startup")
-def obstacle():
-    if ultraS.distance() < 100:
-        wait(10)
-        robot.straight(-100)
-        robot.turn(120)
-        robot.straight(300)
-        robot.turn(45)
-        while rColour == WHITE:
-            robot.straight(300)
-    elif rColour == BLACK:
-        robot.stop()
+#def move():
+ #   while True:
+  #      if lColour == BLACK and rColour == BLACK:
+   #         robot.drive(DRIVE_SPEED)
+    #    elif not lColour == BLACK and not rColour == BLACK:
+     #       path = findPath()
+      #      if path:
+       #         pass
+        #    else:
+         #       break
+       # while lColour == BLACK and rColour == BLACK:
+        #    pass
+
+#def obstacle():
+ #   if ultraS.distance() < 100:
+  #      wait(10)
+   #     robot.straight(-100)
+    #    robot.turn(120)
+     #   robot.straight(300)
+      #  robot.turn(45)
+       # while rColour == WHITE:
+        #    robot.straight(300)
+    #elif rColour == BLACK:
+     #   robot.stop()
+
+while True:
+   if lColour > BLACK:
+       robot.move(5)
