125981b23c92a8e75e1837e7cfaa993c56829be0
will-da-pro <56065795+will-da-pro@users.noreply.github.com>
2022-03-26 11:55:21 +1100

    e

--- main.py
+++ main.py
@@ -48,44 +48,41 @@ def calib():
     
 
 
-ev3.calib()
 
-#def findPath():
- #   robot.stop()
-  #  time = 0
-   # while True:
-    #    if time >= 360:
-     #       return False
-      #  robot.turn(1, True)
-       # if lColour == BLACK or rColour == BLACK:
-        #    return True
-        #time += 1       
+def findPath():
+    robot.stop()
+    time = 0
+    while True:
+        if time >= 360:
+            return False
+        robot.turn(1, True)
+        if lColour == BLACK or rColour == BLACK:
+            return True
+        time += 1       
         
-#def move():
- #   while True:
-  #      if lColour == BLACK and rColour == BLACK:
-   #         robot.drive(DRIVE_SPEED)
-    #    elif not lColour == BLACK and not rColour == BLACK:
-     #       path = findPath()
-      #      if path:
-       #         pass
-        #    else:
-         #       break
-       # while lColour == BLACK and rColour == BLACK:
-        #    pass
+def move():
+    while True:
+        if lColour == BLACK and rColour == BLACK:
+            robot.drive(DRIVE_SPEED)
+        elif not lColour == BLACK and not rColour == BLACK:
+            path = findPath()
+            if path:
+                pass
+            else:
+                break
+        while lColour == BLACK and rColour == BLACK:
+            pass
 
-#def obstacle():
- #   if ultraS.distance() < 100:
-  #      wait(10)
-   #     robot.straight(-100)
-    #    robot.turn(120)
-     #   robot.straight(300)
-      #  robot.turn(45)
-       # while rColour == WHITE:
-        #    robot.straight(300)
-    #elif rColour == BLACK:
-     #   robot.stop()
+def obstacle():
+    if ultraS.distance() < 100:
+        wait(10)
+        robot.straight(-100)
+        robot.turn(120)
+        robot.straight(300)
+        robot.turn(45)
+        while rColour == WHITE:
+            robot.straight(300)
+    elif rColour == BLACK:
+        robot.stop()
 
-while True:
-   if lColour > BLACK:
-       robot.move(5)
+ev3.move(300)
