53f04e17cdd20de8787544dcbe0a8e5b42f1f26c
will-da-pro <56065795+will-da-pro@users.noreply.github.com>
2022-05-15 09:36:50 +1000

    Annotated code

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import time
 ev3 = EV3Brick()
 time_secs = time
 
-
+#Initialize all robot variables
 lColour = ColorSensor(Port.S1)
 rColour = ColorSensor(Port.S4)
 lMotor = Motor(Port.A)
@@ -25,8 +25,8 @@ claw = Motor(Port.C)
 ultraS = UltrasonicSensor(Port.S3)
 robot = DriveBase(lMotor, rMotor, wheel_diameter=55.5, axle_track=120) #to check next week
 
+#Other variables
 ultraSLimit = 100
-
 SILVER = 90
 DRIVE_SPEED = 100
 TURN_DRIVE_SPEED = 60
@@ -35,57 +35,69 @@ BLACK = 20
 
 # Write your program here.
 
-
+#Runs when one of the colour sensors detects black
 def turn(side, degrees):
+    #Starts a timer
     startTime = time_secs.time()
-    print(str(time_secs.time()) + ", "+ str(startTime))
     while side.reflection() <= BLACK:
         robot.drive(TURN_DRIVE_SPEED, degrees)
+        #Afer 0.2 seconds, it starts turning faster.
         if time_secs.time() - startTime >= 0.2:
-            print("Worked!")
             robot.drive(TURN_DRIVE_SPEED, degrees * 2.3) 
 
+#Rotates until a path is detected.             
 def findPath():
     robot.stop()
+    #Each time the robot turns, it will increase this variable
     runTime = 0
     while True:
-        if (runTime >= 360):
+        #If the robot has failed to locate a path after turning 360 degrees, it will return false stopping the program
+        if (runTime >= 36):
             return False
         robot.turn(10)
-        if (lColour.reflection() <= BLACK or rColour.reflection() <= BLACK):
+        #If either sensor detects white, it will return to the move function and continue normally
+        if (lColour.reflection() >= BLACK or rColour.reflection() >= BLACK):
             return True
-        runTime += 1    
+        runTime += 1 
 
-def move():
-    while True:
-
-        if (ultraS.distance() < ultraSLimit):
-            #obstacle()
-            pass
-        if lColour.reflection() >= BLACK and rColour.reflection() >= BLACK:
-            robot.drive(DRIVE_SPEED, 0)
-        elif lColour.reflection() <= BLACK:
-            turn(lColour, -60)
-        elif rColour.reflection() <= BLACK:
-            turn(rColour, 60)
-        else:
-            path = findPath()
-            if path:
-                pass
-            else:
-                break
-
+#Runs if an obstacle is detected
 def obstacle():
-    wait(10)
+    wait(5)
     robot.straight(-100)
     robot.turn(120)
     robot.straight(300)
     while lColour.reflection() <= BLACK and rColour.reflection() <= BLACK:
         pass
 
+#Handles all movement
+def move():
+    while True:
+        if (ultraS.distance() < ultraSLimit):
+            #obstacle()
+            pass
+        #If both sensors detect white, the robot moves in a straight line
+        if lColour.reflection() >= BLACK and rColour.reflection() >= BLACK:
+            robot.drive(DRIVE_SPEED, 0)
+        #If the left sensor detects black, then the robot will turn left
+        elif lColour.reflection() <= BLACK:
+            turn(lColour, -60)
+        #If the right sensor detects black, then the robot will turn right
+        elif rColour.reflection() <= BLACK:
+            turn(rColour, 60)
+        #If both sensors detect black, then the find path function will run
+        else:
+            path = findPath()
+            if path:
+                #If a path is found, the function will continue normally
+                pass
+            else:
+                #If no path is found then the function will break
+                break
+
         
 ev3.speaker.say("HELLLO MR DHARMA")
 
+#Start movement
 move()
 
     
